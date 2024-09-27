8f12ea101516bda9fd61982930e28aa3186f13b0
VinTechG2628 <vintech.g2628@gmail.com>
2024-09-27 17:01:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const apiKey = "ZvhBgz5qrVAOMG6jxRhxhg==ieuqpec0nYdZ8XJe"
+const apiUrl = "https://api.api-ninjas.com/v1/quotes?category=success";
+
+async function getquote(url) {
+    const response = await fetch(url, {
+        headers: {
+            'X-Api-Key': apiKey
+        }
+    });
+
+    const data = await response.json();
+
+    quote.innerHTML = data[0].quote;
+    author.innerHTML = data[0].author;
+}
+
+getquote(apiUrl);
+
+
+function twit() {
+    window.open(`https://twitter.com/intent/tweet?text=${quote.innerHTML}`, "Twit Window", "width = 640px, height = 480px")
+}
